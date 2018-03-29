5bd983f91e155612dfe1875476863b840f1a6ab8
liujingxue <523624469@qq.com>
2018-03-29 17:08:36 +0800

    新增文

---
+++ DOC/1.开始.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Administrator on 2018/3/29.
+ */
+/*
+一、	NodeJS安装
+    版本：9.8.0
+
+二、	NPM配置国内源
+
+    2-1、第一种
+    安装cnpm
+    npm install –g cnpm --registry=https://registry.npm.taobao.org
+    2.2、修改NodeJS的配置文件
+    Windows下打开NodeJS安装目录下node_modules\npm\.npmrc
+	Mac下打开 ~下的.npmrc
+    加上这句话
+    registry=https://registry.npm.taobao.org
+    以后所有的命令都使用npm就行，
+
+三、使用NPM配置React环境
+    npm init –y
+    安装依赖
+    npm install --save react
+
+*/
